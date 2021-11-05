b9bd9b7c2b4a61d641daef64ac944ee6dea25ce7
Maciej Milewski <macias.argedius@gmail.com>
2021-11-05 09:44:23 +0100

    Worki in progress

---
+++ src/domain.js
@@ -0,0 +1,4 @@
+import {icdName} from "./http.js";
+
+
+icdName("99.97901").then(name => console.log(`Twoja nazwa to ${name}`));
---
+++ test/unit/example.test.js
@@ -0,0 +1,11 @@
+import {icdName} from "../../src/http";
+
+
+test('should do something', async () => {
+  // given
+  const code = "99.97901";
+  // when
+  const name = await icdName(code);
+  // then
+  expect(name).toBe('Farmakoterapia doszpikowa');
+});
