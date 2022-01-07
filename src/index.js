b551ef47c96038ddb8e1d2b69bd8e986642b3838
Maciej Milewski <macias.argedius@gmail.com>
2022-01-07 20:15:39 +0100

    Add /authenticate

--- src/index.js
+++ src/index.js
@@ -1,90 +1,103 @@
-import {icdName, patient, patientCreate, patientDelete, patients, workingHours, workingHoursCreate} from "./http";
+import {authenticate, icdName, patient, patientCreate, patientDelete, patients, workingHours, workingHoursCreate} from "./http";
 
 byId("load_working_hours").addEventListener("click", () => {
   workingHours()
-      .then(workingHours => {
-        byId("working_hours").innerText = JSON.stringify(workingHours);
-      })
-      .catch(e => {
-        const s = e.response.status;
-        if (s === 404) alert("Working hours have not been set");
-      });
-})
+    .then(workingHours => {
+      byId("working_hours").innerText = JSON.stringify(workingHours);
+    })
+    .catch(e => {
+      const s = e.response.status;
+      if (s === 404) alert("Working hours have not been set");
+    });
+});
+
+byId("authenticate").addEventListener("click", () => {
+  const [username, password] = prompt('Podaj login:hasło', 'root:root').split(':');
+  authenticate(username, password)
+    .then(response => {
+      byId("authentication").innerText = JSON.stringify(response);
+    })
+    .catch(e => {
+      if (e.response.status === 401) {
+        alert("Błędne dane logowania");
+      }
+    });
+});
 
 byId("save_working_hours").addEventListener("click", () => {
-  const start = prompt("Podaj początek pracy:", "10:00")
-  const duration = prompt("Podaj czas trwania pracy:", "6")
+  const start = prompt("Podaj początek pracy:", "10:00");
+  const duration = prompt("Podaj czas trwania pracy:", "6");
   workingHoursCreate(start, duration)
-      .catch(e => {
-        const s = e.response.status;
-        if (s === 422) alert("Niepoprawny format godzin");
-      });
-})
+    .catch(e => {
+      const s = e.response.status;
+      if (s === 422) alert("Niepoprawny format godzin");
+    });
+});
 
 byId("load_icd").addEventListener("click", () => {
   icdName(prompt("Podaj kod icd", "99.97901"))
-      .then(name => {
-        byId("icd").innerText = JSON.stringify({name});
-      })
-      .catch(e => {
-        const s = e.response.status;
-        if (s === 422) alert("Invalid ICD code");
-        if (s === 404) alert("Unknown ICD code");
-      });
-})
+    .then(name => {
+      byId("icd").innerText = JSON.stringify({name});
+    })
+    .catch(e => {
+      const s = e.response.status;
+      if (s === 422) alert("Invalid ICD code");
+      if (s === 404) alert("Unknown ICD code");
+    });
+});
 
 byId("load_patient_by_pesel").addEventListener("click", () => {
-  const pesel = prompt("Podaj pesel:", "00301000015")
+  const pesel = prompt("Podaj pesel:", "00301000015");
 
   patient(pesel)
-      .then(patient => {
-        byId("patient_by_pesel").innerText = JSON.stringify(patient);
-      });
-})
+    .then(patient => {
+      byId("patient_by_pesel").innerText = JSON.stringify(patient);
+    });
+});
 
 byId("save_patient").addEventListener("click", () => {
-  const pesel = prompt("Podaj pesel:", "00301000015")
-  const name = prompt("Podaj imię i nazwisko:", "Tadeusz Wiśniewski")
+  const pesel = prompt("Podaj pesel:", "00301000015");
+  const name = prompt("Podaj imię i nazwisko:", "Tadeusz Wiśniewski");
 
   patientCreate(pesel, name)
-      .catch(e => {
-        const s = e.response.status;
-        if (s === 422) alert("Niepoprawny pesel");
-        if (s === 409) alert("Pacjent z podanym peselem już istnieje");
-      });
-})
+    .catch(e => {
+      const s = e.response.status;
+      if (s === 422) alert("Niepoprawny pesel");
+      if (s === 409) alert("Pacjent z podanym peselem już istnieje");
+    });
+});
 
 byId("delete_patient").addEventListener("click", () => {
-  const pesel = prompt("Podaj pesel:", "00301000015")
+  const pesel = prompt("Podaj pesel:", "00301000015");
 
   patientDelete(pesel)
-      .catch(e => {
-        const s = e.response.status;
-        if (s === 404) alert("Brak pacjenta z podanym peselem");
-        if (s === 422) alert("Niepoprawny pesel");
-      });
-})
+    .catch(e => {
+      const s = e.response.status;
+      if (s === 404) alert("Brak pacjenta z podanym peselem");
+      if (s === 422) alert("Niepoprawny pesel");
+    });
+});
 
 byId("load_patients").addEventListener("click", () => {
-  const size = prompt("Podaj size:", "2")
-  const page = prompt("Podaj stronę:", "1")
+  const size = prompt("Podaj size:", "2");
+  const page = prompt("Podaj stronę:", "1");
 
   patients(size, page)
-      .then(patient => {
-        byId("patients").innerText = JSON.stringify(patient);
-      })
-      .catch(e => {
-        const s = e.response.status;
-        if (s === 422) alert("Niepoprawne parametry");
-      });
-})
+    .then(patient => {
+      byId("patients").innerText = JSON.stringify(patient);
+    })
+    .catch(e => {
+      const s = e.response.status;
+      if (s === 422) alert("Niepoprawne parametry");
+    });
+});
 
 function byId(id) {
-  const element = document.getElementById(id)
+  const element = document.getElementById(id);
   if (element === null) {
-    throw "Failed to find element by id"
+    throw "Failed to find element by id";
   }
-  return element
+  return element;
 }
 
 
