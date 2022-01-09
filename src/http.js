83c6aa146c51054e0392dbb45acb578a6909da40
Maciej Milewski <macias.argedius@gmail.com>
2022-01-09 18:27:18 +0100

    Add Client class into src/http/

--- src/http.js
+++ src/http.js
@@ -1,48 +1,63 @@
 import axios from "axios";
 import {url} from "./url.js";
 
+class Client {
+  constructor(host, port) {
+    this.axios = axios.create({baseURL: `http://${host}:${port}/`});
+  }
 
-const instance = axios.create({
-  baseURL: 'http://localhost:8080/'
-});
+  get(uri, query = {}) {
+    return this.axios.get(uri, {params: query});
+  }
+
+  post(uri, data) {
+    return this.axios.post(uri, data);
+  }
+
+  delete(uri) {
+    return this.axios.delete(uri);
+  }
+}
+
+const instance = new Client('localhost', 8080);
 
 export function icdName(icdCode) {
-  return instance.get('/icd', {params: {icd: icdCode}})
-      .then(response => response.data["name"]);
+  return instance.get('/icd', {icd: icdCode})
+    .then(response => response.data["name"]);
 }
 
 export function workingHours() {
   return instance.get('/working-hours')
-      .then(response => response.data)
+    .then(response => response.data);
 }
 
 export function workingHoursCreate(start, duration) {
   return instance.post('/working-hours', {start, duration})
-      .then(response => response.data)
+    .then(response => response.data);
 }
 
 export function events(year, month, day) {
   return instance.get('/events', {params: {year, month, day}})
-      .then(response => response.data);
+    .then(response => response.data);
 }
 
 export function patient(pesel) {
   return instance.get(url('/patients/:pesel', {pesel}))
-      .then(response => response.data);
+    .then(response => response.data);
 }
 
 export function patientCreate(pesel, name) {
   return instance.post('/patients', {pesel, name})
-      .then(response => response.data);
+    .then(response => response.data);
 }
 
 export function patientDelete(pesel) {
   return instance.delete(url('/patients/:pesel', {pesel}))
-      .then(response => response.data);
+    .then(response => response.data);
 }
 
 export function patients(size, page) {
-  return instance.get('/patients', {params: {size, page}})
+  return instance.get('/patients', {size, page})
     .then(response => response.data);
 }
 
