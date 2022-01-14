5bfacc1a7b241b7c62d5f5c7917469c43ea4f728
Maciej Milewski <macias.argedius@gmail.com>
2022-01-14 15:25:13 +0100

    Split the view into two entry points: view and develop

---
+++ src/view/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Application from "./src/Application.jsx";
+
+ReactDOM.render(
+  <Application/>,
+  document.getElementById('application')
+);
