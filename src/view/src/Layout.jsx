1a93e13de6e0099518971f4594a865b5de0945b7
Maciej Milewski <macias.argedius@gmail.com>
2022-01-18 23:03:26 +0100

    Add link to "/" in "Ward" in <nav/>

--- src/view/src/Layout.jsx
+++ src/view/src/Layout.jsx
@@ -10,7 +10,9 @@ export default function Layout({children}) {
   return <div>
     <nav className="h-12 bg-blue-400 mb-3 shadow-md">
       <div className="h-12 container text-white mx-auto flex items-center justify-between">
-        <p className="text-xl uppercase">Ward</p>
+        <p className="text-xl uppercase">
+          <Link href="/">Ward</Link>
+        </p>
         <LanguageSwitch/>
       </div>
     </nav>
