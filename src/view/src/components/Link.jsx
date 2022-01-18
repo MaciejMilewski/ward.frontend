8542f46e0b02562c30451ceaeb23e021742b5226
Maciej Milewski <macias.argedius@gmail.com>
2022-01-18 23:43:42 +0100

    <Link/> ignores missing {href} property

--- src/view/src/components/Link.jsx
+++ src/view/src/components/Link.jsx
@@ -2,5 +2,8 @@ import React from "react";
 import {Link as RouterLink} from "react-router-dom";
 
 export default function Link({href, children}) {
-  return <RouterLink to={href}>{children}</RouterLink>;
+  if (href) {
+    return <RouterLink to={href}>{children}</RouterLink>;
+  }
+  return children;
 }
