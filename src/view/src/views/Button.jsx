5de51320cfc05e56bde457e93eef201140ffcd47
Maciej Milewski <macias.argedius@gmail.com>
2022-01-19 00:18:37 +0100

    Link hours update form

--- src/view/src/views/Button.jsx
+++ src/view/src/views/Button.jsx
@@ -1,10 +1,10 @@
+import React from "react";
 import Link from "../components/Link.jsx";
 import Text from "../components/Text.jsx";
-import React from "react";
 
-export default function Button({href, children}) {
+export default function Button({href, onClick, children}) {
   return <Link href={href}>
-    <button className="bg-blue-400 text-white px-3 py-1 rounded-md hover:bg-blue-500 active:mt-1">
+    <button className="bg-blue-400 text-white px-3 py-1 rounded-md hover:bg-blue-500 active:mt-1" onClick={onClick}>
       <Text>{children}</Text>
     </button>
   </Link>;
--- src/view/src/views/hours/Hours.jsx
+++ src/view/src/views/hours/Hours.jsx
@@ -1,28 +1,139 @@
 import React, {useEffect, useState} from "react";
-import {budget, workingHours} from "../../../../http.js";
+import {workingHours, workingHoursCreate} from "../../../../http.js";
+import Text from "../../components/Text.jsx";
+import Button from "../Button.jsx";
 
 export default function Hours({}) {
-  const [loading, setLoading] = useState(true);
-  const [_hours, setHours] = useState(null);
+  const [hours, setHours] = useState(null);
 
   useEffect(() => {
     workingHours()
       .then(_hours => {
         setHours(_hours);
-        setLoading(false);
       })
       .catch(error => {
         if (error.response.status === 404) {
-          setLoading(false);
-          setHours(null);
+          setHours(false);
         }
       })
   }, []);
 
+  function storeHours(start, duration) {
+    workingHoursCreate(start, duration / 60);
+  }
+
   return <div>
-    Working hours:
+    <h3 className="text-xl mb-6">
+      <Text>Working hours</Text>
+    </h3>
     <div>
-      {loading ? 'Loading...' : JSON.stringify({hours: _hours})}
+      <HoursSetForm hours={hours} onSave={storeHours}/>
     </div>
   </div>;
+}
+
+function HoursSetForm({hours, onSave}) {
+  if (hours === null) {
+    return 'Loading...';
+  }
+  if (hours === false) {
+    return <div>
+      Hours not set
+
+      <NewHoursSection
+        initialStart={7 * 60}
+        initialDuration={8 * 60}
+        onSave={onSave}
+      />
+    </div>;
+  }
+
+  return <div>
+    <HoursDisplay start={hours.start} duration={hours.duration}/>
+
+    <NewHoursSection
+      initialStart={hours.start}
+      initialDuration={hours.duration}
+      onSave={onSave}
+    />
+  </div>;
+}
+
+function NewHoursSection({initialDuration, initialStart, onSave}) {
+  const [start, setStart] = useState(initialStart);
+  const [duration, setDuration] = useState(initialDuration);
+
+  return <>
+    <h3 className="text-xl mt-12 mb-4">
+      <Text>New working hours</Text>
+    </h3>
+    <div>
+      <p className="mt-3 mb-2">
+        <Text>Work start</Text>
+      </p>
+      <p>
+        <MinuteField value={start} onChange={setStart}/>, <TimeFormat minutes={start}/>h
+      </p>
+      <p className="mt-3 mb-2">
+        <Text>Work duration</Text>
+      </p>
+      <p>
+        <MinuteField value={duration} onChange={setDuration}/>, <Duration minutes={duration}/>
+      </p>
+      <div className="mt-4">
+        <Button onClick={() => onSave(start, duration)}>
+          Save
+        </Button>
+      </div>
+    </div>
+  </>;
+}
+
+function MinuteField({value, onChange}) {
+  return <>
+    <input className="border w-14" type="number" value={value} min={0} onChange={event => {
+      const minimalTime = Math.max(event.target.value, 0);
+      const maximalTime = Math.min(minimalTime, 60 * 24 - 1);
+      return onChange(maximalTime);
+    }}/>
+    <Text>minutes</Text>
+  </>;
+}
+
+function HoursDisplay({duration, start}) {
+  return <div>
+    <p>
+      <Text>Work start</Text>: <TimeFormat minutes={start}/>
+    </p>
+    <p>
+      <Text>Work duration</Text>: <Duration minutes={duration}/>
+    </p>
+  </div>
+}
+
+function TimeFormat({minutes}) {
+  const [hours, _minutes] = parsed(minutes);
+  return <>
+    {hours}:{padding(_minutes)}
+  </>
+}
+
+function Duration({minutes}) {
+  const [hours, _minutes] = parsed(minutes);
+  return <>{hours} <Text>hours and</Text> {_minutes} <Text>minutes</Text></>;
+}
+
+function padding(value) {
+  const string = "" + value;
+  if (string.length === 1) {
+    return '0' + string;
+  }
+  return string;
+}
+
+function parsed(minutes) {
+  return [
+    parseInt(Math.floor(minutes / 60.0)),
+    minutes % 60
+  ];
 }
