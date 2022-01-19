541b93bc1e097e8fcfead2b23c94c6b64040f177
Maciej Milewski <macias.argedius@gmail.com>
2022-01-20 00:12:24 +0100

    Calls for creating operators

--- src/view/src/views/Field.jsx
+++ src/view/src/views/Field.jsx
@@ -1,11 +1,14 @@
 import Text, {translated} from "../components/Text.jsx";
 import React from "react";
 
-export default function Field({label}) {
+export default function Field({label, value, onChange}) {
   return <>
     <p className="text-sm">
       <label><Text>{label}</Text></label>
     </p>
-    <input className="px-2 py-1 border " placeholder={translated(label)}/>
+    <input className="px-2 py-1 border"
+           value={value}
+           placeholder={translated(label)}
+           onChange={event => onChange(event.target.value)}/>
   </>;
 }
--- src/view/src/views/rooms/NewRoom.jsx
+++ src/view/src/views/rooms/NewRoom.jsx
@@ -1,18 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import Text from "../../components/Text.jsx";
 import Button from "../Button.jsx";
 import Field from "../Field.jsx";
+import {roomCreate} from "../../../../http.js";
+import Link from "../../components/Link.jsx";
 
 export default function NewRoom({}) {
+  const [name, setName] = useState('');
+
+  function create() {
+    roomCreate(name)
+      .catch(error => {
+        if (error.response.status === 442) {
+          alert("Invalid name");
+        }
+        if (error.response.status === 409) {
+          alert("Duplicate room name");
+        }
+      })
+  }
+
   return <article>
+    <nav className="text-sm">
+      <Link href="/rooms">&lt; <Text>Operation rooms</Text></Link>
+    </nav>
     <h3 className="text-xl mb-6">
       <Text>New room</Text>
     </h3>
 
-    <Field label="Room name"/>
+    <Field label="Room name" value={name} onChange={setName}/>
 
     <div className="mt-6">
-      <Button>
+      <Button onClick={create}>
         Create
       </Button>
     </div>
